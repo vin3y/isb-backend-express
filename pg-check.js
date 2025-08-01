85f3c49b8fc2b6d30d8789635cfb7101a5dd76b0
vin3y <vinaysankar7@gmail.com>
2025-08-02 01:47:17 +0530

    Added partner addition and lisiting

---
+++ pg-check.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+
+async function generateHash() {
+    const password = 'admin123';
+    const hash = await bcrypt.hash(password, 10);
+    console.log('Password:', password);
+    console.log('Hash:', hash);
+    console.log('\nSQL to update:');
+    console.log(`UPDATE users SET password_hash = '${hash}' WHERE email = 'admin@isb.com';`);
+}
+
+generateHash();
--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pageController = require("../controllers/pageController");
-const {uploadBackgroundVideo} = require("../utils/s3");
+const partnerController = require("../controllers/homeController");
+const {uploadBackgroundVideo, uploadPartnerLogo} = require("../utils/s3");
 const {authenticateToken} = require("../middlewares/auth");
 
 
@@ -13,6 +14,14 @@ router.patch('/:pageName/status',authenticateToken, pageController.updatePageSta
 router.patch('/:pageName/title',authenticateToken, pageController.updatePageTitle);
 router.post('/:pageName/background',authenticateToken, uploadBackgroundVideo.single('video'), pageController.uploadBackgroundVideo);
 router.post('/:pageName/generate-thumbnail',authenticateToken, pageController.generatePageThumbnail);
+
+//partner home page get routes
+router.get('/home/allpartners', authenticateToken, partnerController.getAllPartners);
+
+// Partner routes (homepage specific)
+router.post('/home/partners',authenticateToken, uploadPartnerLogo.single('logo'), partnerController.addPartner);
+router.put('/home/partners/:partnerId',authenticateToken, uploadPartnerLogo.single('logo'), partnerController.updatePartner);
+router.delete('/home/partners/:partnerId',authenticateToken, partnerController.deletePartner);
 
 
 module.exports = router;
